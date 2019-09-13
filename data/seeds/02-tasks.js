844b6d7f13a4b2e32fa72282c4417266052e8d1d
Chris Jones <chrisjones@Chriss-MacBook-Pro.local>
2019-09-13 17:14:18 +0100

    WIP: ran seed files, checked database setup in sqlitestudio

--- data/seeds/02-tasks.js
+++ data/seeds/02-tasks.js
@@ -1,11 +1,11 @@
 exports.seed = function(knex) {
   return knex('tasks').insert([
     // project_id must match valid id for a project
-    { description: 'st des1', notes: 'st n1', completed: false, project_id: 1 },
-    { description: 'st des2', notes: 'st n2', completed: true, project_id: 2 },
-    { description: 'st des2', completed: false, project_id: 2 },
-    { description: 'st des3', notes: 'st n3', completed: false, project_id: 3 },
-    { description: 'st des3', completed: false, project_id: 3 },
-    { description: 'st des3', notes: 'st n3', completed: true, project_id: 3 }
+    { description: 'task des1', notes: 'task n1', completed: false, project_id: 1 },
+    { description: 'task des2', notes: 'task n2', completed: true, project_id: 2 },
+    { description: 'task des2', completed: false, project_id: 2 },
+    { description: 'task des3', notes: 'task n3', completed: false, project_id: 3 },
+    { description: 'task des3', completed: false, project_id: 3 },
+    { description: 'task des3', notes: 'task n3', completed: true, project_id: 3 }
   ])
 }
